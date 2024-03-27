93cbfb814167a700ccb5ff1ddcf0b69cf523bda9
Rahmatilla1 <xikmatillr@gmail.com>
2024-03-27 16:15:36 +0500

    create package

---
+++ index.js
@@ -0,0 +1,11 @@
+const reverseStr = (str) => {
+    let result = "";
+    for (const letter of str) {
+        result = letter + result
+    }
+
+    return result
+}
+
+
+module.exports = reverseStr
